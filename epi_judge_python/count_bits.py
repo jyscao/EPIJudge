3070d5ca39652b5a14dee9da58bf5aada10a0008
Jethro Cao <jethrocao@gmail.com>
2023-11-06 11:24:42 -0600

    List both solutions for 4 bootcamp example Q's

--- epi_judge_python/count_bits.py
+++ epi_judge_python/count_bits.py
@@ -1,15 +1,23 @@
 from test_framework import generic_test
 
 
-def count_bits(x: int) -> int:
+def count_bits_brute(x: int) -> int:
+    num_bits = 0
+    while x:
+        num_bits += x & 1
+        x >>= 1
+    return num_bits
+
+
+def count_bits_drop(x: int) -> int:
     num_bits = 0
     while x:
         num_bits += 1
-        x &= x - 1
+        x &= x - 1  # drop the lowest set bit of x
     return num_bits
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('count_bits.py', 'count_bits.tsv',
-                                       count_bits))
+    generic_test.generic_test_main('count_bits.py', 'count_bits.tsv', count_bits_brute)
+    print()
+    exit(generic_test.generic_test_main('count_bits.py', 'count_bits.tsv', count_bits_drop))
