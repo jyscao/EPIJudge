6250004de98a9a72066749b9c2a1c7d9daa63f38
Jethro Cao <jethrocao@gmail.com>
2023-11-07 13:56:39 -0600

    Add own & book's search_list func (recursive vs iterative)

--- epi_judge_python/search_in_list.py
+++ epi_judge_python/search_in_list.py
@@ -2,13 +2,23 @@ from list_node import ListNode
 from test_framework import generic_test
 
 
-def search_list(L: ListNode, key: int) -> ListNode:
-    # TODO - you fill in here.
-    return ListNode()
+def search_list_recurse(L: ListNode, key: int) -> ListNode:
+    if L.data == key:
+        return L
+    else:
+        return ListNode(-1, None) if L.next is None else search_list_recurse(L.next, key)
+
+
+def search_list_iter(L:ListNode, key: int) -> ListNode:
+    while L and L.data != key:
+        L = L.next
+    else:
+        return L
 
 
 def search_list_wrapper(L, key):
-    result = search_list(L, key)
+    # result = search_list_recurse(L, key)
+    result = search_list_iter(L, key)
     return result.data if result else -1
 
 
