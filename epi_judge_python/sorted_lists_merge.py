77ee6d9cda0c2c896b8e01599721c6cd08fb55b5
Jethro Cao <jethrocao@gmail.com>
2023-11-07 15:03:25 -0600

    Add own soln for sorted_lists_merge (kinda messy)

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -6,8 +6,30 @@ from test_framework import generic_test
 
 def merge_two_sorted_lists(L1: Optional[ListNode],
                            L2: Optional[ListNode]) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    if L1 is None:
+        return L2
+    elif L2 is None:
+        return L1
+
+    current, other = (L1, L2) if L1.data <= L2.data else (L2, L1)
+    head = current
+    while True:
+        next_on_curr = current.next
+        if next_on_curr is None:
+            current.next = other
+            break
+
+        if next_on_curr.data <= other.data:
+            current = next_on_curr
+        else:
+            current.next, other = other, current.next
+
+        if current.next is None:
+            current.next = other
+            break
+
+    return head
+
 
 
 if __name__ == '__main__':
