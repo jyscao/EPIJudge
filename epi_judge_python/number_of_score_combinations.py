bd3395d2d5b50cb6e9429d299e8b7707ce2e3754
Jethro Cao <jethrocao@gmail.com>
2023-11-08 15:58:48 -0600

    Copy soln for 16.1 from book (need to study more)

--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -2,11 +2,24 @@ from typing import List
 
 from test_framework import generic_test
 
+import numpy as np
+
 
 def num_combinations_for_final_score(final_score: int,
                                      individual_play_scores: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+# def num_combinations_for_final_score(final_score, individual_play_scores):
+    # individual_play_scores.sort()     # NOTE: sorting is not needed
+    scores_combo = np.zeros((len(individual_play_scores), final_score + 1,), dtype=int)
+    for p in range(len(individual_play_scores)):
+        play_score = individual_play_scores[p]
+        for s in range(0, final_score + 1):
+            if s == 0:
+                scores_combo[p, 0] = 1
+            else:
+                without_play = scores_combo[p - 1, s] if p >= 1 else 0
+                with_play = scores_combo[p, s - play_score] if s >= play_score else 0
+                scores_combo[p, s] = without_play + with_play
+    return scores_combo[-1, -1]
 
 
 if __name__ == '__main__':
