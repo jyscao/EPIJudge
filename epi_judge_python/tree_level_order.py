bedac6f899a152bf2593ba7d6bb3aac0ffb834a0
Jethro Cao <jethrocao@gmail.com>
2023-11-07 22:32:04 -0600

    Add solns for tree_level_order
    book's is slightly cleaner; but they're basically the same

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -5,8 +5,36 @@ from test_framework import generic_test
 
 
 def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    if not tree:
+        return []
+
+    result = []
+    curr_level_nodes, next_level_nodes = [tree], []
+    while curr_level_nodes:
+        result.append([node.data for node in curr_level_nodes])
+        for node in curr_level_nodes:
+            if node.left:
+                next_level_nodes.append(node.left)
+            if node.right:
+                next_level_nodes.append(node.right)
+        curr_level_nodes, next_level_nodes = next_level_nodes, []
+
+    return result
+
+
+def binary_tree_depth_order_book(tree: BinaryTreeNode) -> List[List[int]]:
+    result = []
+    if not tree:
+        return result
+    curr_depth_nodes = [tree]
+    while curr_depth_nodes:
+        result.append([curr.data for curr in curr_depth_nodes])
+        curr_depth_nodes = [child
+            for node in curr_depth_nodes
+            for child in (node.left, node.right)
+            if child
+        ]
+    return result
 
 
 if __name__ == '__main__':
@@ -14,3 +42,4 @@ if __name__ == '__main__':
         generic_test.generic_test_main('tree_level_order.py',
                                        'tree_level_order.tsv',
                                        binary_tree_depth_order))
+                                       # binary_tree_depth_order_book))
