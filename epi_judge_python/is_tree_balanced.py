7eebd69e9263f5ad599377026fabc31dd054166a
Jethro Cao <jethrocao@gmail.com>
2023-11-08 20:45:11 -0600

    Copy book soln for is_tree_balanced (needs more practice)

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -3,8 +3,27 @@ from test_framework import generic_test
 
 
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    is_balanced, _height = check_balanced(tree)
+    # print()
+    # print(_height)
+    return is_balanced
+
+
+def check_balanced(tree):
+    if not tree:
+        return True, -1
+
+    left_balanced, left_height = check_balanced(tree.left)
+    if not left_balanced:
+        return False, None
+
+    right_balanced, right_height = check_balanced(tree.right)
+    if not right_balanced:
+        return False, None
+
+    is_balanced = abs(left_height - right_height) <= 1
+    height = max(left_height, right_height) + 1
+    return is_balanced, height
 
 
 if __name__ == '__main__':
