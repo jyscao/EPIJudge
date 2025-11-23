eec03a30aa61933c4d0cf0468e0286d34fe4e8e2
Jethro Cao <jethrocao@gmail.com>
2025-11-22 21:34:40 -0500

    save stashed

--- epi_judge_python/binary_tree_node.py
+++ epi_judge_python/binary_tree_node.py
@@ -16,3 +16,24 @@ class BinaryTreeNode:
 
     def __str__(self):
         return self.__repr__()
+
+
+# TODO/FIXME: the tree element's order are fucked up
+def build_bt_from_list(bt_list):
+    bt_list_rev = list(reversed(bt_list))
+
+    curr_depth_nodes = [BinaryTreeNode(data=bt_list_rev.pop()[0])]
+    ans = curr_depth_nodes[0]
+    while bt_list_rev:
+        next_depth_data = bt_list_rev.pop()
+        assert 2 * len(curr_depth_nodes) == len(next_depth_data)
+
+        next_depth_nodes = []
+        for i, node in enumerate(curr_depth_nodes):
+            node.left = BinaryTreeNode(data=next_depth_data[2*i])
+            node.right= BinaryTreeNode(data=next_depth_data[2*i+1])
+            next_depth_nodes.append(node.left)
+            next_depth_nodes.append(node.right)
+        curr_depth_nodes = next_depth_nodes
+
+    return ans
